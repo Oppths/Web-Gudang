b16f12b09d6ad44d930bc3b20eef5f316ac23aec
Deva Indrayana Rahmat <112385806+Oppths@users.noreply.github.com>
2022-12-07 11:58:53 +0700

    Add files via upload

---
+++ Web_gudang.sql
@@ -0,0 +1,232 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 06 Des 2022 pada 17.48
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `web_gudang`
+--
+CREATE DATABASE IF NOT EXISTS `web_gudang` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `web_gudang`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_barang_keluar`
+--
+
+CREATE TABLE `tb_barang_keluar` (
+  `id` int(10) NOT NULL,
+  `id_transaksi` varchar(50) NOT NULL,
+  `tanggal_masuk` varchar(20) NOT NULL,
+  `tanggal_keluar` varchar(20) NOT NULL,
+  `lokasi` varchar(100) NOT NULL,
+  `kode_barang` varchar(100) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `satuan` varchar(50) NOT NULL,
+  `jumlah` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tb_barang_keluar`
+--
+
+INSERT INTO `tb_barang_keluar` (`id`, `id_transaksi`, `tanggal_masuk`, `tanggal_keluar`, `lokasi`, `kode_barang`, `nama_barang`, `satuan`, `jumlah`) VALUES
+(1, 'WG-201713067948', '8/11/2017', '11/11/2017', 'NTB', '8888166995215', 'Ciki Walens', 'Dus', '50'),
+(2, 'WG-201713067948', '8/11/2017', '11/12/2017', 'NTB', '8888166995215', 'Ciki Walens', 'Dus', '6'),
+(3, 'WG-201713549728', '4/11/2017', '11/11/2017', 'Banten', '1923081008002', 'Buku Hiragana', 'Pack', '3'),
+(4, 'WG-201774896520', '9/11/2017', '12/11/2017', 'Yogyakarta', '60201311121008264', 'Battery ZTE', 'Dus', '3'),
+(5, 'WG-201727134650', '05/12/2017', '20/12/2017', 'Jakarta', '29312390203', 'Susu', 'Dus', '17'),
+(6, 'WG-201810974628', '15/01/2018', '16/01/2018', 'Lampung', '1923081008002', 'Buku Nihongo', 'Dus', '50'),
+(7, 'WG-201781267543', '7/11/2017', '17/01/2018', 'Yogyakarta', '97897952889', 'Buku Framework Codeigniter', 'Dus', '1'),
+(8, 'WG-201832570869', '15/01/2018', '17/01/2018', 'Bali', '1923081008002', 'test', 'Dus', '10'),
+(9, 'WG-201893850472', '15/01/2018', '18/01/2018', 'Bali', '1923081008002', 'lumpur nartugo', 'Pcs', '11'),
+(10, 'WG-201781267543', '7/11/2017', '15/01/2018', 'Yogyakarta', '97897952889', 'Buku Framework Codeigniter', 'Dus', '1'),
+(11, 'WG-201727134650', '05/12/2017', '15/01/2018', 'Jakarta', '29312390203', 'Susu', 'Dus', '3'),
+(12, 'WG-201774896520', '9/11/2017', '15/01/2018', 'Yogyakarta', '60201311121008264', 'Battery ZTE', 'Dus', '3'),
+(13, 'WG-201727134650', '05/12/2017', '16/01/2018', 'Jakarta', '29312390203', 'Susu', 'Dus', '1'),
+(14, 'WG-201727134650', '05/12/2017', '17/01/2018', 'Jakarta', '29312390203', 'Susu', 'Dus', '1'),
+(15, 'WG-201885472106', '18/01/2018', '19/01/2018', 'Riau', '8996001600146', 'Teh Pucuk', 'Dus', '50'),
+(16, 'WG-201871602934', '18/01/2018', '16/03/2018', 'Papua', '312212331222', 'Kopi Hitam', 'Dus', '10');
+
+--
+-- Trigger `tb_barang_keluar`
+--
+DELIMITER $$
+CREATE TRIGGER `TG_BARANG_KELUAR` AFTER INSERT ON `tb_barang_keluar` FOR EACH ROW BEGIN
+ UPDATE tb_barang_masuk SET jumlah=jumlah-NEW.jumlah
+ WHERE kode_barang=NEW.kode_barang;
+ DELETE FROM tb_barang_masuk WHERE jumlah = 0;
+
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_barang_masuk`
+--
+
+CREATE TABLE `tb_barang_masuk` (
+  `id_transaksi` varchar(50) NOT NULL,
+  `tanggal` varchar(20) NOT NULL,
+  `lokasi` varchar(100) NOT NULL,
+  `kode_barang` varchar(100) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `satuan` varchar(50) NOT NULL,
+  `jumlah` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tb_barang_masuk`
+--
+
+INSERT INTO `tb_barang_masuk` (`id_transaksi`, `tanggal`, `lokasi`, `kode_barang`, `nama_barang`, `satuan`, `jumlah`) VALUES
+('WG-201871602934', '18/01/2018', 'Papua', '312212331222', 'Kopi Hitam', 'Dus', '100');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_satuan`
+--
+
+CREATE TABLE `tb_satuan` (
+  `id_satuan` int(11) NOT NULL,
+  `kode_satuan` varchar(100) NOT NULL,
+  `nama_satuan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tb_satuan`
+--
+
+INSERT INTO `tb_satuan` (`id_satuan`, `kode_satuan`, `nama_satuan`) VALUES
+(1, 'Dus', 'Dus'),
+(2, 'Pcs', 'Pcs'),
+(5, 'Pack', 'Pack');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_upload_gambar_user`
+--
+
+CREATE TABLE `tb_upload_gambar_user` (
+  `id` int(11) NOT NULL,
+  `username_user` varchar(100) NOT NULL,
+  `nama_file` varchar(220) NOT NULL,
+  `ukuran_file` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tb_upload_gambar_user`
+--
+
+INSERT INTO `tb_upload_gambar_user` (`id`, `username_user`, `nama_file`, `ukuran_file`) VALUES
+(4, 'admin', 'nopic2.png', '6.33'),
+(5, 'user', 'nopic2.png', '6.33');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(12) NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `role` tinyint(4) NOT NULL DEFAULT 0,
+  `last_login` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `role`, `last_login`) VALUES
+(20, 'admin', 'admin@gmail.com', '$2y$10$3HNkMOtwX8X88Xb3DIveYuhXScTnJ9m16/rPDF1/VTa/VTisxVZ4i', 1, '06-12-2022 16:44'),
+(22, 'user', 'user@gmail.com', '$2y$10$rd/Kjm0VjVDg5D8m7WNsGOPQL5aMDA8mnhCzAz03Htp6jf0x9AGLq', 0, '06-12-2022 16:26');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_barang_keluar`
+--
+ALTER TABLE `tb_barang_keluar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_barang_masuk`
+--
+ALTER TABLE `tb_barang_masuk`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- Indeks untuk tabel `tb_satuan`
+--
+ALTER TABLE `tb_satuan`
+  ADD PRIMARY KEY (`id_satuan`);
+
+--
+-- Indeks untuk tabel `tb_upload_gambar_user`
+--
+ALTER TABLE `tb_upload_gambar_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_barang_keluar`
+--
+ALTER TABLE `tb_barang_keluar`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_satuan`
+--
+ALTER TABLE `tb_satuan`
+  MODIFY `id_satuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_upload_gambar_user`
+--
+ALTER TABLE `tb_upload_gambar_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
